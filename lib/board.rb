9fa08bc31bbcb39190de2d873b07b58f44419e2c
diegoarvz4 <darvz.gtz@gmail.com>
2019-05-27 13:42:55 -0500

    Board initial properties added

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,40 @@
 
+class Board
+
+    attr_accessor :dimensions
+    attr_accessor :turn_type
+
+    def initialize(turn_type)
+        @dimensions = []
+        for i in 0..2
+            dimensions << []
+            for j in 0..2
+                dimensions[i] << " "
+            end
+        end 
+
+        @turn_type = turn_type
+
+    end 
+
+    
+    def display
+        for row in 0..2
+            for col in 0..2
+                print "[" + "#{dimensions[row][col]}"+"]"
+            end 
+            puts ""
+        end
+    end 
+
+    def set_cell=(value)
+
+        row = value[0].to_i
+        col = value[1].to_i
+        
+        dimensions[row][col] = @turn_type
+
+    end
+
+end
 
