05327d6987071d05c26646d939de9e916b78c817
diegoarvz4 <darvz.gtz@gmail.com>
2019-06-10 15:24:36 -0500

    Final updates

--- lib/match.rb
+++ lib/match.rb
@@ -42,37 +42,51 @@ class Match
 
 
     def game_over?
-        winner? || tie?
+        board = @board.dimensions
+        winner?(board) || tie?(board)
     end 
      
 
-    def winner?
-        board = @board.dimensions
+    def winner?(board)
+        return true if check_horizontal_winning(board)
+        return true if check_vertical_winning(board)
+        return true if cehck_diagonal_winning(board)
+        false
+    end 
+
+    def tie?(board)
+        if board.is_full? 
+            @winner_type = "TIE"
+            return true 
+        end 
+    end 
+
+    def check_horizontal_winning(board)
         #vertical and horizonal equality checks
         j = 0
         for i in 0...3
             return true if (board[j]==board[j+1])&&(board[j+1]==board[j+2])
             j += 3
         end
-       #verticals equality test
+        false 
+    end 
+
+    def check_vertical_winning(board)
+        #verticals equality test
         j = 0
         for i in 0...3
             return true if (board[j]==board[j+3])&&(board[j+3]==board[j+6])
             j += 1
         end
-       #diagonals equality test
+        false 
+    end 
+
+    def cehck_diagonal_winning(board)
+        #diagonals equality test
         return true if (board[0]==board[4])&&(board[4]==board[8])
         return true if (board[2]==board[4])&&(board[4]==board[6]) 
-        false
+        false 
     end 
-
-    def tie?
-        if @board.is_full? 
-            @winner_type = "TIE"
-            return true 
-        end 
-    end 
-
 
     def decide_winner_or_tie
 
